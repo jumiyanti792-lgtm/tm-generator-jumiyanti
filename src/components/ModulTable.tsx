1cc9ebf5417351c8c66a839ff840691bd5ba91e4
jumiyanti792-lgtm <jumiyanti792@guru.sd.belajar.id>
2026-05-23 10:03:57 +0800

    Update ModulTable.tsx

--- src/components/ModulTable.tsx
+++ src/components/ModulTable.tsx
@@ -151,8 +151,8 @@ export default function ModulTable({ data, formInput, onBack }: ModulTableProps)
                   <td>{data.identitas.subject}</td>
                 </tr>
                 <tr>
-                  <td className="font-semibold">Semester</td>
-                  <td>{data.identitas.classSemester?.replace(/Kelas\s*/i, '')}</td>
+                  <td className="font-semibold">Kelas/Semester</td>
+                  <td>{data.identitas.classSemester?.replace(/Kelas\s*/i, '').trim()}</td>
                 </tr>
                 <tr>
                   <td className="font-semibold">Jumlah Pertemuan</td>
